be9b0b1997c7bedae409d422a4f66750dfa7b299
Jose Cordoba <jose.m.cordoba.c@gmail.com>
2018-11-03 11:17:45 -0500

    Mejoras en la versión de intervalos

--- Tutoria3/scripts.js
+++ Tutoria3/scripts.js
@@ -1,1 +1,8 @@
-alert('Hola mundo');
+var miFuncion = function(){
+           alert('Hola mundo');
+    contenido = document.getElementById('contenido');
+    console.log(contenido);
+    contenido.innerHTML = 'cambio despues de 2 segundos'
+
+           };
+setTimeout(miFuncion, 2000);
